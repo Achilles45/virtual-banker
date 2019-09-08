5453621d5e0d70c2622cf639ff867511ecf8c14c
Achilles <achillesusuoyibo7@gmail.com>
2019-09-08 15:06:23 +0100

    Scafolded register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="register__wrapper">
         <Navbar />
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis dolore odio animi enim officia fugit, et quisquam aut ipsam natus ad sint dignissimos assumenda consequatur facere dicta fuga sed. Laboriosam voluptate ab soluta at ad nemo, qui omnis explicabo saepe earum alias itaque perferendis repellendus a magnam sunt, exercitationem temporibus. Obcaecati, libero omnis quam ipsum consectetur eum. Ipsam accusantium assumenda quisquam molestias, veritatis laboriosam perspiciatis ut illo animi? Ab animi rem modi vel qui quibusdam debitis autem culpa sequi doloribus corrupti dignissimos beatae aspernatur dolorum tenetur, incidunt obcaecati sed mollitia. Nisi praesentium neque aperiam earum eaque error, laudantium voluptatem molestias.
     </div>
 </template>
 <script>
