66482abe37c9d723c485f56b2b439df0df3e6b41
Achilles <achillesusuoyibo7@gmail.com>
2019-09-08 14:33:23 +0100

    Starter pack for register

---
+++ src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="register__wrapper">
+        <Navbar />
+    </div>
+</template>
+<script>
+import Navbar from '@components/Navbar.vue';
+export default {
+    name: 'Register',
+    components:{
+        Navbar
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '../styles/_colors';
+</style>
